04bf175422dfb86cb17efffc9905e4efd6c6cd02
a5116107 <153204139+a5116107@users.noreply.github.com>
2026-02-06 18:52:05 +0800

    chore(sync): align toolbar rule button and remove dead account helper

--- frontend/src/views/admin/AccountsView.vue
+++ frontend/src/views/admin/AccountsView.vue
@@ -16,16 +16,6 @@
             @sync="showSync = true"
             @create="showCreate = true"
           >
-            <template #before>
-              <button
-                @click="showErrorPassthrough = true"
-                class="btn btn-secondary"
-                :title="t('admin.errorPassthrough.title')"
-              >
-                <Icon name="shield" size="md" class="mr-1.5" />
-                <span class="hidden md:inline">{{ t('admin.errorPassthrough.title') }}</span>
-              </button>
-            </template>
             <template #after>
               <!-- Auto Refresh Dropdown -->
               <div class="relative" ref="autoRefreshDropdownRef">
@@ -69,6 +59,16 @@
                   </button>
                 </div>
               </div>
+
+              <!-- Error Passthrough Rules -->
+              <button
+                @click="showErrorPassthrough = true"
+                class="btn btn-secondary"
+                :title="t('admin.errorPassthrough.title')"
+              >
+                <Icon name="shield" size="md" class="mr-1.5" />
+                <span class="hidden md:inline">{{ t('admin.errorPassthrough.title') }}</span>
+              </button>
 
               <!-- Column Settings Dropdown -->
               <div class="relative" ref="columnDropdownRef">
